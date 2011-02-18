7398528f35fc52177a0e386b7c5fed81c3d4922e
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-02-18 11:35:15 +0100

    updated long_description

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(name='collective.js.jqsmartTruncation',
       maintainer=maintainer,
       version=version,
       description="truncate too long text",
-      long_description=open("README.txt").read() + "\n" + \
-          open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read() + "\n" +
+                         open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
