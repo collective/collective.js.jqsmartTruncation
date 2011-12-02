51d69a496b796552b3dba95fe93a85f3ce33bc8a
Jonas Baumann <tschouns@gmail.com>
2011-12-02 09:22:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a3dev'
+version = '1.0dev'
 maintainer = 'Elio Schmutz'
 
 setup(name='collective.js.jqsmartTruncation',
       version=version,
-      description="This Package provides smart Truncation.",
-      long_description=open("README.rst").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="This Package provides smart truncation.",
+      long_description=open("README.rst").read() + "\n" + \
+            open(os.path.join("docs", "HISTORY.txt")).read(),
+            
       classifiers=[
-        
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='jquery smartTruncation',
       author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       license='GPL2',
       url='https://github.com/collective/collective.js.jqsmartTruncation',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js'],
       include_package_data=True,
